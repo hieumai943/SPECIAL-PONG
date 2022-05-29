36c3f992bd6a6180497e051c679a9f58768cc0e8
Hieu <21020069@vnu.edu.vn>
2022-05-29 21:19:58 +0700

    code

--- main.cpp
+++ main.cpp
@@ -8,12 +8,11 @@
     Game game;
 int main(int argc, char* args[]){
      game.restart=1;
-while(game.restart==1){
+while(game.restart){
      game.variable();
      game.serve();
      game.runMenu();
-   if(game.run!=1)
-     game.rungame();
+     if(game.run!=1)  game.rungame();
      game.runback();
      game.start=0;
   }
