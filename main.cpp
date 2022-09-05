3ba79795496f862d01127b07910beeb24436da79
hieumai943 <hieumai09042003@gmail.com>
2022-09-05 18:08:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
-#include"object.h"
-#include"game.h"
-#include"menu.h"
+#include "object.h"
+#include "game.h"
+#include "menu.h"
 
-    SDL_Renderer *renderer;
-    SDL_Window *window;
-    TTF_Font* font;
-    Game game;
-int main(int argc, char* args[]){
-     game.restart=1;
-while(game.restart){
-     game.variable();
-     game.serve();
-     game.runMenu();
-     if(game.run!=1)  game.rungame();
-     game.runback();
-     game.start=0;
-  }
- }
+SDL_Renderer *renderer;
+SDL_Window *window;
+TTF_Font *font;
+Game game;
+int main(int argc, char *args[])
+{
+     game.restart = 1;
+     while (game.restart)
+     {
+          game.variable();
+          game.serve();
+          game.runMenu();
+          if (game.run != 1)
+               game.rungame();
+          game.runback();
+          game.start = 0;
+     }
+}
